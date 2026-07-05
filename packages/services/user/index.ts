b1de5ba63a3ae6de894e5c72c77237a49593bb5a
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-05 11:31:03 +0500

    feat: add users table and update environment configuration for OAuth

--- packages/services/user/index.ts
+++ packages/services/user/index.ts
@@ -1,29 +1,7 @@
-import { db } from "@repo/database";
-import { usersTable } from "@repo/database/schema";
-import { env } from "../env";
-import { googleOAuth2Client } from "../clients/google-oauth";
-import { GetAuthenticationMethodOutputSchema } from "./model";
+class userService {
 
-class UserService {
-  public async getAuthenticationMethods(): Promise<
-    ReadonlyArray<GetAuthenticationMethodOutputSchema>
-  > {
-    const supportedAuthenticationProviders: GetAuthenticationMethodOutputSchema[] = [];
+    public createUserWithEmailAndPassword(){}
 
-    const isGoogleConfigured = !!(env.GOOGLE_OAUTH_CLIENT_ID && env.GOOGLE_OAUTH_CLIENT_SECRET);
-
-    if (isGoogleConfigured) {
-      const url = googleOAuth2Client.generateAuthUrl();
-      supportedAuthenticationProviders.push({
-        provider: "GOOGLE_OAUTH",
-        displayName: "Google",
-        displayText: "Signin with Google",
-        authUrl: url,
-      });
-    }
-
-    return supportedAuthenticationProviders;
-  }
 }
 
-export default UserService;
+export default userService;
