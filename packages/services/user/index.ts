52279f6d65c0010143fa81f1a522125f1c5acad6
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-06 10:08:41 +0500

    feat: add salt and password fields to users table and implement user creation logic

--- packages/services/user/index.ts
+++ packages/services/user/index.ts
@@ -1,6 +1,27 @@
+import {db, eq} from "@repo/database"
+import {usersTable} from "@repo/database/models/user"
+import {randomBytes , createHmac} from "node:crypto"
+import {CreateUserWithEmailAndPasswordInputType , createUserWithEmailAndPasswordInput}from "./model"
+
 class userService {
 
-    public createUserWithEmailAndPassword(){}
+    private async getUserByEmil(email : string){
+     const result =  await db.select().from(usersTable).where(eq(usersTable.email , email))
+     if(!result || result.length === 0) return null
+     return result[0]
+    }
+
+    private async createUserWithEmailAndPassword(payload : CreateUserWithEmailAndPasswordInputType ){
+         const {fullName , email , password} = await createUserWithEmailAndPasswordInput.parseAsync(payload)
+
+         //check if the user exist or not
+         const existingUserWithEmail = await this.getUserByEmil(email)
+         if(existingUserWithEmail) throw new Error(`User with ${email} already exists`)
+
+         const salt = randomBytes(16).toString("hex")
+         const hash = createHmac("sha256" , salt).update(password).digest("hex")
+         
+    }
 
 }
 
---
+++ packages/services/user/model.ts
@@ -0,0 +1,9 @@
+import {z} from "zod"
+
+export const createUserWithEmailAndPasswordInput = z.object({
+    fullName : z.string().describe("Full Name of the user"),
+    email : z.email().describe("Email Address of the user"),
+    password : z.string().describe("Password of the user")
+});
+
+export type CreateUserWithEmailAndPasswordInputType = z.infer<typeof createUserWithEmailAndPasswordInput>
