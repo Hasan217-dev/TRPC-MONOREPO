7b3c7741bc93d74e4f25a3ec5b2ed59c15b9ad95
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-06 10:14:39 +0500

    feat: implement user creation logic with password hashing and salt generation

--- packages/services/user/index.ts
+++ packages/services/user/index.ts
@@ -18,11 +18,20 @@ class userService {
          const existingUserWithEmail = await this.getUserByEmil(email)
          if(existingUserWithEmail) throw new Error(`User with ${email} already exists`)
 
+        //calculate salt and hash the password
          const salt = randomBytes(16).toString("hex")
          const hash = createHmac("sha256" , salt).update(password).digest("hex")
          
-    }
+         //create userI in Db
+         const userInsertResult = await db.insert(usersTable).values({fullName , email , password : hash , salt}).returning({
+            id : usersTable.id
+         });
+         if(!userInsertResult || userInsertResult.length === 0) throw new Error(`something went wrong while creating the user`)
 
+            return {
+                id : userInsertResult[0]?.id
+            }
+    }
 }
 
 export default userService;
