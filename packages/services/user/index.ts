35be1b81fda3a96a2a2615946c7dbd052fa5a17e
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-06 10:17:57 +0500

    fix: correct class name and method name typos in UserService

--- packages/services/user/index.ts
+++ packages/services/user/index.ts
@@ -3,9 +3,9 @@ import {usersTable} from "@repo/database/models/user"
 import {randomBytes , createHmac} from "node:crypto"
 import {CreateUserWithEmailAndPasswordInputType , createUserWithEmailAndPasswordInput}from "./model"
 
-class userService {
+class UserService {
 
-    private async getUserByEmil(email : string){
+    private async getUserByEmail(email : string){
      const result =  await db.select().from(usersTable).where(eq(usersTable.email , email))
      if(!result || result.length === 0) return null
      return result[0]
@@ -15,14 +15,14 @@ class userService {
          const {fullName , email , password} = await createUserWithEmailAndPasswordInput.parseAsync(payload)
 
          //check if the user exist or not
-         const existingUserWithEmail = await this.getUserByEmil(email)
+         const existingUserWithEmail = await this.getUserByEmail(email)
          if(existingUserWithEmail) throw new Error(`User with ${email} already exists`)
 
         //calculate salt and hash the password
          const salt = randomBytes(16).toString("hex")
          const hash = createHmac("sha256" , salt).update(password).digest("hex")
          
-         //create userI in Db
+         //create user in Db
          const userInsertResult = await db.insert(usersTable).values({fullName , email , password : hash , salt}).returning({
             id : usersTable.id
          });
@@ -34,4 +34,4 @@ class userService {
     }
 }
 
-export default userService;
+export default UserService
