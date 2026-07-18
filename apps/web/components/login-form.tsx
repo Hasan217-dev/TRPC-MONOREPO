9d29140f7b60793b861437201d2a76ea9e7f5710
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-18 11:31:08 +0500

    feat: implement login and signup pages with forms and authentication hooks

---
+++ apps/web/components/login-form.tsx
@@ -0,0 +1,135 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { cn } from "~/lib/utils"
+import { Button } from "~/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card"
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "~/components/ui/field"
+import { Input } from "~/components/ui/input"
+import { useSignIn } from "~/hooks/api/auth"
+import { da } from "zod/v4/locales"
+
+type LoginFormValues = {
+  email: string
+  password: string
+}
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+
+  const {signInUserWithEmailAndPasswordAsync} = useSignIn()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>()
+
+  const onSubmit = async (data: LoginFormValues) => {
+    console.log("Login values:", data)
+   const {id} = await signInUserWithEmailAndPasswordAsync({
+      email : data.email ,
+      password : data.password
+    })
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FieldGroup>
+
+              {/* Email */}
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^\S+@\S+\.\S+$/,
+                      message: "Enter a valid email",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <FieldDescription className="text-destructive">
+                    {errors.email.message}
+                  </FieldDescription>
+                )}
+              </Field>
+
+              {/* Password */}
+              <Field>
+                <div className="flex items-center">
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+
+                <Input
+                  id="password"
+                  type="password"
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 6,
+                      message: "Minimum 6 characters",
+                    },
+                  })}
+                />
+
+                {errors.password && (
+                  <FieldDescription className="text-destructive">
+                    {errors.password.message}
+                  </FieldDescription>
+                )}
+              </Field>
+
+              {/* Buttons */}
+              <Field>
+                <Button type="submit">Login</Button>
+
+                <Button variant="outline" type="button">
+                  Login with Google
+                </Button>
+
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a href="#">Sign up</a>
+                </FieldDescription>
+              </Field>
+
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
