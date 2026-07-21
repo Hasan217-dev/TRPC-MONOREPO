a352ec4d0955b917421b185bb04fb303e0d3cf47
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-21 10:30:14 +0500

    feat: implement createForm mutation with input validation and service integration

--- packages/trpc/server/routes/form/route.ts
+++ packages/trpc/server/routes/form/route.ts
@@ -1,5 +1,37 @@
-import { publicProcedure, router } from "../../trpc";
+import { formService } from "../../services";
+import { authenticatedProcedure, router } from "../../trpc";
+import { generatePath } from "../../utils/path-generator";
+import {
+    createFormInputModel,
+    createFormOutputModel,
+} from "./model"
+
+const TAGS = ["Form"];
+const getPath = generatePath("/form");
 
 export const formRouter = router({
-    
+
+    createForm : authenticatedProcedure
+    .meta({
+        openapi : {
+            method : "POST" ,
+            path : getPath("/createForm") ,
+            tags : TAGS ,
+        }
+    })
+    .input(createFormInputModel)
+    .output(createFormOutputModel)
+    .mutation(async ({ input , ctx }) => {
+        const { title , description } = input
+
+        const { id } = await formService.createForm({
+            title ,
+            description ,
+            createdBy : ctx.user.id ,
+        })
+
+        return {
+            id ,
+        }
+    }) ,
 })
