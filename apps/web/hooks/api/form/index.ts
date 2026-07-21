22a27af5fd0cbf34a0a4cd6bb6de9d36f1f2c492
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-07-21 11:58:29 +0500

    refactor: clean up formatting in useCreateForm and useListForms hooks

--- apps/web/hooks/api/form/index.ts
+++ apps/web/hooks/api/form/index.ts
@@ -2,16 +2,16 @@ import { trpc } from "~/trpc/client"
 
 export const useCreateForm = () => {
     const {
-         mutateAsync: createFormAsync ,
-          mutate : createForm ,
-          error,
-          failureCount,
-          isError,
-          isIdle,
-          isSuccess,
-          status
-          
-        } = trpc.form.createForm.useMutation()
+        mutateAsync: createFormAsync,
+        mutate: createForm,
+        error,
+        failureCount,
+        isError,
+        isIdle,
+        isSuccess,
+        status,
+    } = trpc.form.createForm.useMutation()
+
     return {
         createFormAsync,
         createForm,
@@ -20,6 +20,30 @@ export const useCreateForm = () => {
         isError,
         isIdle,
         isSuccess,
-        status
+        status,
+    }
+}
+
+export const useListForms = () => {
+    const {
+        data: forms,
+        error,
+        isError,
+        isLoading,
+        isPending,
+        refetch,
+        status,
+    } = trpc.form.listForms.useQuery(undefined, {
+        retry: false,
+    })
+
+    return {
+        forms,
+        error,
+        isError,
+        isLoading,
+        isPending,
+        refetch,
+        status,
     }
 }
