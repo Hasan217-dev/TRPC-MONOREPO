af9cb97892f3b46a4a06bb95b40437cda6d4c649
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-08-24 16:01:40 +0500

    setup done

--- apps/web/hooks/api/health.ts
+++ apps/web/hooks/api/health.ts
@@ -2,6 +2,7 @@ import {trpc} from "~/trpc/client"
 
 export function useHealth(){
     const {data , error , isFetched , isFetching , isLoading , status} = trpc.health.getHealth.useQuery();
+    
 
     return {
         data ,
